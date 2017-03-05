4b6975bf98aea7d723a4f4dba623e00ae8174ff5
Ruud Schouten <ruud.schouten@outlook.com>
2017-03-05 13:01:20 +0100

    Added more fields Implemented Parcelable

--- app/src/main/java/com/blappole/instantperusal/Book.java
+++ app/src/main/java/com/blappole/instantperusal/Book.java
@@ -1,13 +1,56 @@
 package com.blappole.instantperusal;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import org.joda.time.Period;
 import org.joda.time.format.PeriodFormat;
 
 import java.util.ArrayList;
 
-class Book {
+class Book implements Parcelable {
     String Name;
-    ArrayList<Chapter> Chapters;
+    String Author;
+    String Year;
+    int Pages;
+
+    private ArrayList<Chapter> Chapters;
+
+    //region Parcelable
+    protected Book(Parcel in) {
+        Name = in.readString();
+        Author = in.readString();
+        Year = in.readString();
+        Pages = in.readInt();
+        Chapters = in.createTypedArrayList(Chapter.CREATOR);
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(Name);
+        parcel.writeString(Author);
+        parcel.writeString(Year);
+        parcel.writeInt(Pages);
+        parcel.writeTypedList(Chapters);
+    }
+    //endregion
 
     Book(String name) {
         Name = name;
@@ -16,14 +59,6 @@ class Book {
 
     void addChapter(Chapter c) {
         Chapters.add(c);
-    }
-
-    int getPages() {
-        int pages = 0;
-        for (Chapter c : Chapters) {
-            pages += c.Pages;
-        }
-        return pages;
     }
 
     String getTimeRead() {
