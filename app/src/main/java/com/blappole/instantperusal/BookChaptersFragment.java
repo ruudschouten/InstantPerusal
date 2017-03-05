3f85a38f0d3d52b393833372a2aee360f0d377ac
Ruud Schouten <ruud.schouten@outlook.com>
2017-03-05 13:58:44 +0100

    Added BookChaptersFragment

---
+++ app/src/main/java/com/blappole/instantperusal/BookChaptersFragment.java
@@ -0,0 +1,34 @@
+package com.blappole.instantperusal;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+public class BookChaptersFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_book_chapters, container, false);
+
+        ButterKnife.bind(this, v);
+
+        if(savedInstanceState == null) {
+            savedInstanceState = getArguments();
+        }
+
+        Book book = savedInstanceState.getParcelable("book");
+        assert book != null;
+        return v;
+    }
+
+    public static BookChaptersFragment newInstance(Book book) {
+        BookChaptersFragment fragment = new BookChaptersFragment();
+        Bundle bundle =  new Bundle();
+        bundle.putParcelable("book", book);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+}
