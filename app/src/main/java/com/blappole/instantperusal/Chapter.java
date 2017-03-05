af1fa939f0d55f6b469f0a05cc1d11bdc84c4f30
Ruud Schouten <ruud.schouten@outlook.com>
2017-03-05 13:01:04 +0100

    Implemented Parcelable

--- app/src/main/java/com/blappole/instantperusal/Chapter.java
+++ app/src/main/java/com/blappole/instantperusal/Chapter.java
@@ -1,13 +1,48 @@
 package com.blappole.instantperusal;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import org.joda.time.*;
 
-class Chapter {
+class Chapter implements Parcelable {
     String Name;
     int Pages;
     Period TimeSpent;
 
     private LocalDateTime startTime;
+
+    //region Parcelable
+
+    protected Chapter(Parcel in) {
+        Name = in.readString();
+        Pages = in.readInt();
+    }
+
+    public static final Creator<Chapter> CREATOR = new Creator<Chapter>() {
+        @Override
+        public Chapter createFromParcel(Parcel in) {
+            return new Chapter(in);
+        }
+
+        @Override
+        public Chapter[] newArray(int size) {
+            return new Chapter[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(Name);
+        parcel.writeInt(Pages);
+        parcel.writeLong(TimeSpent.getMillis());
+    }
+    //endregion
 
     Chapter(String name, int pages) {
         Name = name;
