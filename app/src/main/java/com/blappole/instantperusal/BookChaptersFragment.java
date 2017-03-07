2df57fd13365869fb3972f5448f4bcec32c57670
ruudschouten <l12345l>
2017-03-07 09:14:20 +0100

    Added fab

--- app/src/main/java/com/blappole/instantperusal/BookChaptersFragment.java
+++ app/src/main/java/com/blappole/instantperusal/BookChaptersFragment.java
@@ -1,18 +1,24 @@
 package com.blappole.instantperusal;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
+
+
+
 public class BookChaptersFragment extends Fragment {
+    @BindView(R.id.fabAdd) FloatingActionButton fabAdd;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_book_chapters, container, false);
-
         ButterKnife.bind(this, v);
 
         if(savedInstanceState == null) {
