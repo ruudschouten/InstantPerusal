f83e36d74b378b756107447f4a5ea8f7a7c2117c
Ruud Schouten <ruud.schouten@outlook.com>
2017-03-07 20:29:41 +0100

    It is now possible to add new chapters

--- app/src/main/java/com/blappole/instantperusal/BookChaptersFragment.java
+++ app/src/main/java/com/blappole/instantperusal/BookChaptersFragment.java
@@ -1,20 +1,29 @@
 package com.blappole.instantperusal;
 
+import android.app.Dialog;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Interpolator;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-
-
-
 public class BookChaptersFragment extends Fragment {
     @BindView(R.id.fabAdd) FloatingActionButton fabAdd;
+    @BindView(R.id.frChaptersEmpty) TextView tvEmpty;
+    @BindView(R.id.frChapterContainer) ListView lvChapters;
+    Book book;
+    ChapterArrayAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,9 +34,41 @@ public class BookChaptersFragment extends Fragment {
             savedInstanceState = getArguments();
         }
 
-        Book book = savedInstanceState.getParcelable("book");
+        book = savedInstanceState.getParcelable("book");
         assert book != null;
+
+        adapter = new ChapterArrayAdapter(v.getContext(), R.layout.chapter_layout, book.Chapters);
+        lvChapters.setAdapter(adapter);
+        lvChapters.setEmptyView(tvEmpty);
+
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAddChapterDialog(view);
+            }
+        });
         return v;
+    }
+
+    public void showAddChapterDialog(View v) {
+        final Dialog dialog = new Dialog(v.getContext());
+        dialog.setContentView(R.layout.add_chapter_dialog);
+        final Button dialogButton = (Button) dialog.findViewById(R.id.btnAddChapter);
+        final EditText bookName = (EditText) dialog.findViewById(R.id.editChapterName);
+        final EditText bookAuthor = (EditText) dialog.findViewById(R.id.editChapterPages);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = bookName.getText().toString();
+                String pages = bookAuthor.getText().toString();
+
+                Chapter c = new Chapter(name, Integer.valueOf(pages));
+                book.addChapter(c);
+                adapter.notifyDataSetChanged();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 
     public static BookChaptersFragment newInstance(Book book) {
